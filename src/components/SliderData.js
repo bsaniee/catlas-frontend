25fc9416572ed7ded98565892891e99c0460a1d9
Bijan Saniee <bsaniee@gmail.com>
2021-12-15 18:56:00 -0500

    Image Slider data added

---
+++ src/components/SliderData.js
@@ -0,0 +1,23 @@
+export const SliderData = [
+    {
+      image: 'https://i.imgur.com/6gU9m8O.png'
+    },
+    {
+      image: 'https://toppng.com/uploads/preview/cat-png-clip-art-persian-cat-11563021167ehnldfnnqf.png'
+    },
+    {
+      image: 'https://e7.pngegg.com/pngimages/300/309/png-clipart-cat-bastet-goddess-egyptian-mythology-ancient-egypt-cat-animals-cat-like-mammal.png'
+    },
+    {
+      image: 'https://image.pngaaa.com/865/4124865-middle.png'
+    },
+    {
+      image: 'https://e7.pngegg.com/pngimages/686/707/png-clipart-british-shorthair-american-shorthair-exotic-shorthair-european-shorthair-kitten-kitten-mammal-cat-like-mammal.png'
+    },
+    {
+      image: 'https://png.pngitem.com/pimgs/s/527-5276324_cat-tuxedo-blackandwhite-sitting-freetoeditnot-tuxedo-cat-png.png'
+    },
+    {
+      image: 'https://spng.pngfind.com/pngs/s/260-2602391_gray-horse-shorthair-dog-british-cat-persian-clipart.png'
+    }
+  ]
