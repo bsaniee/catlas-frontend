7a1a6b4b91b43fe7fad3a24f2c9c222726bfc68f
Bijan Saniee <bsaniee@gmail.com>
2021-12-16 03:26:13 -0500

    Index.js added, css added

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -16,4 +16,121 @@ const Index = (props) => {
 
     const [searchValue, setSearchValue] = useState("")
 
-    
+    const handleChange = (event) => {
+        const newState = {...newForm}
+        newState[event.target.name] = event.target.value
+        setNewForm(newState)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.createCat(newForm)
+        setNewForm({
+            name: "",
+            color: "",
+            breed: "",
+            image: "",
+            description: ""
+        })
+    }
+
+    const form = (
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+            <legend>Add a new Cat to the Catlas!</legend>
+            <div className="flex-pair">
+                <label for="name">Name</label>
+                <input 
+                    type="text"
+                    value={newForm.name}
+                    name="name"
+                    id="name"
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="flex-pair">
+                <label for="color">Color</label>
+                <input 
+                    type="text"
+                    value={newForm.color}
+                    name="color"
+                    id="color"
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="flex-pair">
+                <label for="breed">Breed</label>
+                <input 
+                    type="text"
+                    value={newForm.breed}
+                    name="breed"
+                    id="breed"
+                    onChange={handleChange}
+                />
+            </div> 
+
+            <div className="flex-pair">
+                <label for="image">Cat Photo</label>
+                <input 
+                    type="text"
+                    value={newForm.image}
+                    name="image"
+                    id="image"
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="flex-pair">
+                <label for="description">Description</label>
+                <input 
+                    type="text"
+                    value={newForm.description}
+                    name="description"
+                    id="description"
+                    onChange={handleChange}
+                />
+            </div>
+            </fieldset>
+            <input type="submit" value="Add Cat" id="add"/>
+        </form>
+    )
+    if (props.cats) {
+        return (
+            <section>
+                <ImageSlider slides={SliderData} />
+                {form}
+                <input type="search" placeholder="Search for Cats" onChange={(event) => setSearchValue(event.target.value)}/>
+                <div className="cats-container">
+                    <Fade triggerOnce>
+                    {props.cats.filter((cat) => {
+                        if (searchValue === "") return cat
+                        else if (cat.name.toLowerCase().includes(searchValue.toLowerCase())) return cat
+                        else if (cat.breed.toLowerCase().includes(searchValue.toLowerCase())) return cat
+                    })
+                    .map((cat) => {
+                        return (<div key={cat._id} className="cat">
+                            <Link to={`/cats/${cat._id}`}>
+                                <img src={cat.image}/>
+                                <div className="book-details">
+                                    <h1>{cat.name}</h1>
+                                    <h2>Cat Type {cat.breed}</h2>
+                                </div>
+                            </Link>
+                        </div>)
+                    })}
+                    </Fade>
+                </div>
+            </section>
+        )
+    } else {
+        return (
+            <section>
+                {form}
+                <h1>Loading...</h1>
+            </section>
+        )
+    }
+}
+
+export default Index
